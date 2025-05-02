7d863f682a3bf96b5d44a0b9f864bd811dd07a41
Vishnu Dompale <143195086+The-vishnu@users.noreply.github.com>
2025-05-02 07:23:14 +0000

    changes

--- 5passwordgenerator/src/App.jsx
+++ 5passwordgenerator/src/App.jsx
@@ -5,14 +5,19 @@ import './App.css'
 import './Style.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [lenght, setLength] = useState(8)
+  const [numAllowed, setNumAlloed] = useState(true)
+  const [charAllowed, setCharAlloed] = useState(true)
 
   return (
     <>
-      <div className='w-full h-screen bg-black text-white itme-center justify-center p-0 m-0'>
-        <h1>Password Mennager</h1>
-        <p>Uday Bushettiwar</p>
+      <div className='w-full h-screen itme-center justify-center flex gap-3 bg-red'>
+        <h1 className='text-3xl'>Password Generator</h1>
+        <div className='my-15 absolute w-[100px] border-rose-500'>
+          <input type="text" className='outline-none w-full px-1 py-4' placeholder='password' readOnly/>
+        </div>
       </div>
+
     </>
   )
 }
